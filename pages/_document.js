d6568f65c1f57c28c1b08bab30c18262bc4fbc86
Mohsin Dawood <87421117+mohsindawood@users.noreply.github.com>
2023-04-15 23:52:21 +0500

    designed added

--- pages/_document.js
+++ pages/_document.js
@@ -16,18 +16,6 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/logo.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/logo.png"
-          />
 
           <meta name="theme-color" content={palette.light.primary.main} />
           <link rel="manifest" href="/manifest.json" />
@@ -41,39 +29,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <title>Techgater - Web Design &amp; Development Agency</title>
-          <meta
-            name="msvalidate.01"
-            content="0C312F1E2E203F1B104B546B6682F9ED"
-          />
-          <meta
-            name="title"
-            content="Techgater - Web Design &amp; Development Agency"
-          />
-          <meta name="author" content="Techgater" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <meta name="theme-color" content="#c7ecff" />
-          <meta
-            name="description"
-            content="Techgater is Sargodha, Pakistan Based softwere company providing high-end websites and web app development services."
-          />
-          <meta
-            name="keywords"
-            content="Techgater,Techgater website agency,techgater softwere company, techgater lahore based website agency, techgater sargodha based website agency,lahore based softwere company techgater,Techgater React Developer,Teachgater vue developer, Techgater web development, techgater web app development, techgater agency, website design and development, software company"
-          />
-          <meta property="og:url" content="https://techgater.com/" />
-          <meta property="og:site_name" content="Techgater" />
-          <meta
-            name="og:title"
-            content="Techgater - Web Design &amp; Development Agency"
-          />
-
-          <meta name="twitter:url" content="https://techgater.com/" />
-          <meta name="twitter:site" content="Techgater" />
-          <meta
-            name="twitter:description"
-            content="Techgater is Lahore, Pakistan Based softwere company providing high-end website and web app development services. We build best quality, user-friendly and responsive custom Design Websites & dashboard."
-          />
+          <title>PokerBirds - Web Design &amp; Development Agency</title>
         </Head>
         <body>
           <Main />
